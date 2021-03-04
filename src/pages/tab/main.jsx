9244439cd93418c6a973b134bf647ae0bc57a374
xiedong <xiedong11@aliyun.com>
2021-03-04 21:57:18 +0800

    绘制我的页面布局

--- src/pages/tab/main.jsx
+++ src/pages/tab/main.jsx
@@ -21,7 +21,7 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pagesIndex: 0,
+      pagesIndex: 2,
       pages: [<Home/>, <Data/>, <My/>]
     }
 
--- src/pages/tab/my/index.jsx
+++ src/pages/tab/my/index.jsx
@@ -8,9 +8,25 @@ export default class My extends Component {
 
 
   render() {
-    return(
-      <View>
-        my
+    return (
+      <View id='my-view-root'>
+
+
+        <Text id = 'app-title'>keeping记账系统</Text>
+        <View id='my-item-view'>
+          <Text>修改密码</Text>
+          <Text> > </Text>
+        </View>
+        <View id='my-item-view'>
+          <Text>关于keeping记账系统</Text>
+          <Text> > </Text>
+        </View>
+        <View id='my-item-view'>
+          <Text>意见反馈</Text>
+          <Text> > </Text>
+        </View>
+
+        <Text id='btn-logout'>注销登录</Text>
       </View>
     )
   }
