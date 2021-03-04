4f70f50de2ad0a2360a85e911cc3b2eda5655661
xiedong <xieguodong@chinanews.com.cn>
2021-03-04 11:08:21 +0800

    tab切换选中逻辑

--- src/pages/tab/main.jsx
+++ src/pages/tab/main.jsx
@@ -7,6 +7,12 @@ import './style.less'
 import Home from "./home";
 import Data from "./data";
 import My from "./my";
+import ic_tab_home_selected from '../../image/ic_tab_home_selected.png'
+import ic_tab_home_normal from '../../image/ic_tab_home_normal.png'
+import ic_tab_bill_selected from '../../image/ic_tab_bill_selected.png'
+import ic_tab_bill_normal from '../../image/ic_tab_bill_normal.png'
+import ic_tab_my_selected from '../../image/ic_tab_my_selected.png'
+import ic_tab_my_normal from '../../image/ic_tab_my_normal.png'
 
 
 export default class Main extends Component {
@@ -40,24 +46,23 @@ export default class Main extends Component {
         <View id='bottom-view'>
 
           <View className='bottom-item' onClick={() => this.selectIndex(0)}>
-            {/*<Image src={icKb}></Image>*/}
-            <Text>
+            <Image id ='bottom-tab-img' src={pagesIndex == 0 ? ic_tab_home_selected : ic_tab_home_normal}></Image>
+            <Text id ={pagesIndex == 0 ?"bottom-tab-text-selected":"bottom-tab-text-normal"}>
               首页
             </Text>
           </View>
 
           <View className='bottom-item' onClick={() => this.selectIndex(1)}>
-            {/*<Image src={icKb}></Image>*/}
+            <Image id ='bottom-tab-img' src={pagesIndex == 1 ? ic_tab_bill_selected : ic_tab_bill_normal}></Image>
 
-            <Text>
+            <Text id ={pagesIndex == 1 ?"bottom-tab-text-selected":"bottom-tab-text-normal"}>
               记账
             </Text>
           </View>
 
           <View className='bottom-item' onClick={() => this.selectIndex(2)}>
-            {/*<Image src={icKb}></Image>*/}
-
-            <Text>
+            <Image id ='bottom-tab-img' src={pagesIndex == 2 ? ic_tab_my_selected : ic_tab_my_normal}></Image>
+            <Text id ={pagesIndex == 2 ?"bottom-tab-text-selected":"bottom-tab-text-normal"}>
               我的
             </Text>
           </View>
