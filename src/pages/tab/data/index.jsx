c6affd6a9d01dbd69a5ba45cd84a8c5b4ef70d33
xiedong <xieguodong@chinanews.com.cn>
2021-03-05 16:25:20 +0800

    修改资源文件

--- src/pages/tab/data/index.jsx
+++ src/pages/tab/data/index.jsx
@@ -3,10 +3,39 @@ import React, {Component} from 'react'
 import {Text, View, Image} from "@tarojs/components";
 import "taro-ui/dist/style/components/tab-bar.scss" // 按需引入
 import './style.less'
-import {AtButton, AtInput,AtTextarea} from "taro-ui";
+import {AtButton, AtInput, AtTextarea} from "taro-ui";
 import "taro-ui/dist/style/components/button.scss" // 按需引入
 import "taro-ui/dist/style/components/input.scss" // 按需引入
-import "taro-ui/dist/style/components/textarea.scss" // 按需引入
+import "taro-ui/dist/style/components/textarea.scss"
+import baoxiao from "../../../image/baoxiao.png";
+import baoxiao_select from "../../../image/baoxiao_select.png";
+import gongzi from "../../../image/gongzi.png";
+import gongzi_select from "../../../image/gongzi_select.png";
+import hongbao from "../../../image/hongbao.png";
+import hongbao_select from "../../../image/hongbao_select.png";
+import jiangjinguize from "../../../image/jiangjinguize.png";
+import jiangjinguize_select from "../../../image/jiangjinguize_select.png";
+import lvyou from "../../../image/lvyou.png";
+import lvyou_select from "../../../image/lvyou_select.png";
+import qita from "../../../image/qita.png";
+import qita_select from "../../../image/qita_select.png";
+import riyongpin from "../../../image/riyongpin.png";
+import riyongpin_select from "../../../image/riyongpin_select.png";
+import traffic from "../../../image/traffic.png";
+import traffic_select from "../../../image/traffic_select.png";
+import xuexi from "../../../image/xuexi.png";
+import xuexi_select from "../../../image/xuexi_select.png";
+import yiban from "../../../image/yiban.png";
+import yiban_select from "../../../image/yiban_select.png";
+import yiban1 from "../../../image/yiban_1.png";
+import yiban1_select from "../../../image/yiban_1_select.png";
+import yongcan from "../../../image/yongcanqu.png";
+import yongcan_select from "../../../image/yongcanqu_select.png";
+import yule from "../../../image/yule.png";
+import yule_select from "../../../image/yule_select.png";
+import ziyuan from "../../../image/ziyuan.png";
+import ziyuan_select from "../../../image/ziyuan_select.png";
+
 
 export default class Data extends Component {
   constructor() {
@@ -15,7 +44,7 @@ export default class Data extends Component {
       number: 0,
       moneyType: 0,
       costType: 0,
-      desc:''
+      desc: ''
     }
   }
 
@@ -25,25 +54,28 @@ export default class Data extends Component {
     })
 
   }
+
   handleNumberChange(value) {
     this.setState({
       number: value
     })
 
   }
-  onCostTypeSelected(value){
+
+  onCostTypeSelected(value) {
     this.setState({
-      costType:value
+      costType: value
     })
   }
 
-  onMoneyTypeSelected(value){
+  onMoneyTypeSelected(value) {
     this.setState({
-      moneyType:value
+      moneyType: value,
+      costType: value == 0 ? 0 : 10
     })
   }
 
-  handleClick(){
+  handleClick() {
 
     var number = parseInt(this.state.number)
     var moneyType = this.state.moneyType
@@ -51,7 +83,7 @@ export default class Data extends Component {
     var desc = this.state.desc
 
 
-    if (number.length <1) {
+    if (number.length < 1) {
       Taro.showToast({
         icon: 'none',
         title: '请输入正确金额'
@@ -59,10 +91,10 @@ export default class Data extends Component {
     } else {
 
       let query = Bmob.Query('CostEntity');
-      query.set("number",number)
-      query.set("moneyType",moneyType)
-      query.set("costType",costType)
-      query.set("desc",desc)
+      query.set("number", number)
+      query.set("moneyType", moneyType)
+      query.set("costType", costType)
+      query.set("desc", desc)
       query.save().then(res => {
         console.log(res)
         Taro.showToast({
@@ -71,8 +103,8 @@ export default class Data extends Component {
         })
 
         this.setState({
-          number:0,
-          desc:''
+          number: 0,
+          desc: ''
         })
       }).catch(err => {
         Taro.showToast({
@@ -90,11 +122,17 @@ export default class Data extends Component {
    * moneyType  0 收入  1，支出
    *
    * costType
-   * 0 日常消费  1。人情往来  2 工资  3 交通出行
+   * 收入
+   * 0 学习  1。一般  2 用餐  3 交通 4。日用品 5 娱乐 6旅游 7其他
+   *
+   * 支出
+   * 10 一般 11工资  12红包 13奖金 14投资 15报销 16其他
+   *
    *
    */
   render() {
     let {moneyType, costType} = this.state
+
     return (
       <View id='data-root'>
 
@@ -107,17 +145,86 @@ export default class Data extends Component {
           onChange={this.handleNumberChange.bind(this)}
         />
         <Text className='title-data'>类型</Text>
-        <View className='type-view'>
-          <Text className={moneyType == 0 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onMoneyTypeSelected(0)}>收入</Text>
-          <Text className={moneyType == 1 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onMoneyTypeSelected(1)}>支出</Text>
+        <View className='money-type-view'>
+          <Text className={moneyType == 0 ? 'type-item-selected' : 'type-item-normal'}
+                onClick={() => this.onMoneyTypeSelected(0)}>收入</Text>
+          <Text className={moneyType == 1 ? 'type-item-selected' : 'type-item-normal'}
+                onClick={() => this.onMoneyTypeSelected(1)}>支出</Text>
         </View>
         <Text className='title-data'>消费类别</Text>
-        <View className='type-view'>
-          <Text className={costType == 0 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onCostTypeSelected(0)}>日常消费</Text>
-          <Text className={costType == 1 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onCostTypeSelected(1)}>人情往来</Text>
-          <Text className={costType == 2 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onCostTypeSelected(2)}>工资</Text>
-          <Text className={costType == 3 ? 'type-item-selected' : 'type-item-normal'} onClick={()=>this.onCostTypeSelected(3)}>交通出行</Text>
-        </View>
+
+
+        {
+          moneyType == 0 ?
+            <View className='cost-type-view'>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(0)}>
+                <Image id='meun-img' src={costType == 0 ? xuexi_select : xuexi}></Image>
+                <Text className={costType == 0 ? 'type-item-selected' : 'type-item-normal'}>学习</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(1)}>
+                <Image id='meun-img' src={costType == 1 ? yiban_select : yiban}></Image>
+                <Text className={costType == 1 ? 'type-item-selected' : 'type-item-normal'}>一般</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(2)}>
+                <Image id='meun-img' src={costType == 2 ? yongcan_select : yongcan}></Image>
+                <Text className={costType == 2 ? 'type-item-selected' : 'type-item-normal'}>用餐</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(3)}>
+                <Image id='meun-img' src={costType == 3 ? traffic_select : traffic}></Image>
+                <Text className={costType == 3 ? 'type-item-selected' : 'type-item-normal'}>交通</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(4)}>
+                <Image id='meun-img' src={costType == 4 ? riyongpin_select : riyongpin}></Image>
+                <Text className={costType == 4 ? 'type-item-selected' : 'type-item-normal'}>日用品</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(5)}>
+                <Image id='meun-img' src={costType == 5 ? yule_select : yule}></Image>
+                <Text className={costType == 5 ? 'type-item-selected' : 'type-item-normal'}>娱乐</Text>
+              </View>
+              <View className='item-view'>
+                <Image id='meun-img' src={costType == 6 ? lvyou_select : lvyou}
+                       onClick={() => this.onCostTypeSelected(6)}></Image>
+                <Text className={costType == 6 ? 'type-item-selected' : 'type-item-normal'}>旅游</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(7)}>
+                <Image id='meun-img' src={costType == 7 ? qita_select : qita}></Image>
+                <Text className={costType == 7 ? 'type-item-selected' : 'type-item-normal'}>其他</Text>
+              </View>
+            </View>
+            :
+            <View className='cost-type-view'>
+              <View className='item-view'>
+                <Image id='meun-img' src={costType == 10 ? yiban1_select : yiban1}
+                       onClick={() => this.onCostTypeSelected(10)}></Image>
+                <Text className={costType == 10 ? 'type-item-selected' : 'type-item-normal'}>一般</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(11)}>
+                <Image id='meun-img' src={costType == 11 ? gongzi_select : gongzi}></Image>
+                <Text className={costType == 11 ? 'type-item-selected' : 'type-item-normal'}>工资</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(12)}>
+                <Image id='meun-img' src={costType == 12 ? hongbao_select : hongbao}></Image>
+                <Text className={costType == 12 ? 'type-item-selected' : 'type-item-normal'}>红包</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(13)}>
+                <Image id='meun-img' src={costType == 13 ? jiangjinguize_select : jiangjinguize}></Image>
+                <Text className={costType == 13 ? 'type-item-selected' : 'type-item-normal'}>奖金</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(14)}>
+                <Image id='meun-img' src={costType == 14 ? ziyuan_select : ziyuan}></Image>
+                <Text className={costType == 14 ? 'type-item-selected' : 'type-item-normal'}>投资</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(15)}>
+                <Image id='meun-img' src={costType == 15 ? baoxiao_select : baoxiao}></Image>
+                <Text className={costType == 15 ? 'type-item-selected' : 'type-item-normal'}>报销</Text>
+              </View>
+              <View className='item-view' onClick={() => this.onCostTypeSelected(16)}>
+                <Image id='meun-img' src={costType == 16 ? qita_select : qita}></Image>
+                <Text className={costType == 16 ? 'type-item-selected' : 'type-item-normal'}>其他</Text>
+              </View>
+            </View>
+        }
+
 
         <div id='item-space'/>
         <AtTextarea
--- src/pages/tab/home/index.jsx
+++ src/pages/tab/home/index.jsx
@@ -3,8 +3,21 @@ import React, {Component} from 'react'
 import {Text, View, Image} from "@tarojs/components";
 import "taro-ui/dist/style/components/tab-bar.scss" // 按需引入
 import './style.less'
-import {AtTabs, AtTabsPane} from 'taro-ui'
 import "taro-ui/dist/style/components/tabs.scss";
+import baoxiao_select from "../../../image/baoxiao_select.png";
+import gongzi_select from "../../../image/gongzi_select.png";
+import hongbao_select from "../../../image/hongbao_select.png";
+import jiangjinguize_select from "../../../image/jiangjinguize_select.png";
+import lvyou_select from "../../../image/lvyou_select.png";
+import qita_select from "../../../image/qita_select.png";
+import riyongpin_select from "../../../image/riyongpin_select.png";
+import traffic_select from "../../../image/traffic_select.png";
+import xuexi_select from "../../../image/xuexi_select.png";
+import yiban_select from "../../../image/yiban_select.png";
+import yiban1_select from "../../../image/yiban_1_select.png";
+import yongcan_select from "../../../image/yongcanqu_select.png";
+import yule_select from "../../../image/yule_select.png";
+import ziyuan_select from "../../../image/ziyuan_select.png";
 
 export default class Home extends Component {
 
@@ -42,19 +55,52 @@ export default class Home extends Component {
    */
 
   getCostType(type) {
-    let typeInfo = "日常消费"
+    let typeInfo = "其他"
     switch (type) {
       case 0:
-        typeInfo = "日常消费"
+        typeInfo = "学习"
         break;
       case 1:
-        typeInfo = "人情往来"
+        typeInfo = "一般"
         break;
       case 2:
-        typeInfo = "工资"
+        typeInfo = "用餐"
         break;
       case 3:
-        typeInfo = "交通出行"
+        typeInfo = "交通"
+        break;
+      case 4:
+        typeInfo = "日用品"
+        break;
+      case 5:
+        typeInfo = "娱乐"
+        break;
+      case 6:
+        typeInfo = "旅游"
+        break;
+      case 7:
+        typeInfo = "其他"
+        break;
+      case 10:
+        typeInfo = "一般"
+        break;
+      case 11:
+        typeInfo = "工资"
+        break;
+      case 12:
+        typeInfo = "红包"
+        break;
+      case 13:
+        typeInfo = "奖金"
+        break;
+      case 14:
+        typeInfo = "投资"
+        break;
+      case 15:
+        typeInfo = "报销"
+        break;
+      case 16:
+        typeInfo = "其他"
         break;
     }
 
@@ -64,49 +110,87 @@ export default class Home extends Component {
 
   goDetailPage(item) {
     Taro.navigateTo({
-      url: '/pages/tab/data/detail/index?id='+item.objectId
+      url: '/pages/tab/data/detail/index?id=' + item.objectId
     })
 
 
   }
 
+  getImgType(costType) {
+    let img = qita_select
+
+    switch (costType) {
+      case 0:
+        img = xuexi_select
+        break;
+      case 1:
+        img = yiban_select
+        break;
+      case 2:
+        img = yongcan_select
+        break;
+      case 3:
+        img = traffic_select
+        break;
+      case 4:
+        img = riyongpin_select
+        break;
+      case 5:
+        img = yule_select
+        break;
+      case 6:
+        img = lvyou_select
+        break;
+      case 7:
+        img = qita_select
+        break;
+      case 10:
+        img = yiban1_select
+        break;
+      case 11:
+        img = gongzi_select
+        break;
+      case 12:
+        img = hongbao_select
+        break;
+      case 13:
+        img = jiangjinguize_select
+        break;
+      case 14:
+        img = ziyuan_select
+        break;
+      case 15:
+        img = baoxiao_select
+        break;
+      case 16:
+        img = qita_select
+        break;
+    }
+    return img
+  }
+
   render() {
-    let {costArray, current} = this.state
-    const tabList = [{title: '收入'}, {title: '支出'}]
+
+
+    let {costArray} = this.state
+
     return (
       <View id='home-root'>
-        <AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick.bind(this)}>
-          <AtTabsPane current={this.state.current} index={0}>
-            {
-              ((costArray).filter(item => item.moneyType == 0) || []).map((item, index) => {
-                return <View id='view-shouru' onClick={() => this.goDetailPage(item)}>
-                  <Text id='home-item-title'>支出</Text>
-                  <Text id='item-money-count'>金额 : {item.number}</Text>
-                  <View id='item-bottom-view'>
-                    <Text id='item-money-type'>消费类别 : {this.getCostType(item.costType)}</Text>
-                    <Text id='item-money-date'>{item.createdAt}</Text>
-                  </View>
+        {
+          (costArray || []).map((item, index) => {
+            return <View id='view-shouru' onClick={() => this.goDetailPage(item)}>
+              <View id='item-content-view'>
+                <Image id='shouru-item-icon' src={this.getImgType(item.costType)}/>
+                <Text id='home-item-title'>{this.getCostType(item.costType)}</Text>
+                <Text
+                  id={item.moneyType == 0 ? 'item-money-count-shouru' : 'item-money-count-zhichu'}>{item.number}</Text>
+              </View>
 
-                </View>
-              })
-            }
-          </AtTabsPane>
-          <AtTabsPane current={this.state.current} index={1}>
-            {
-              ((costArray).filter(item => item.moneyType == 1) || []).map((item, index) => {
-                return <View id='view-shouru' onClick={() => this.goDetailPage(item)}>
-                  <Text id='home-item-title'>收入</Text>
-                  <Text id='item-money-count'>金额 : {item.number}</Text>
-                  <View id='item-bottom-view'>
-                    <Text id='item-money-type'>消费类别 : {this.getCostType(item.costType)}</Text>
-                    <Text id='item-money-date'>{item.createdAt}</Text>
-                  </View>
+              <View id='item-bottom-view'/>
 
-                </View>
-              })
-            }
-          </AtTabsPane>
-        </AtTabs>
+            </View>
+          })
+        }
       </View>
     )
   }
